6d655c7ad1512bbb96211437efe62cc4fdcd2b7a
uzakryszhan <uz.ryszhan123@gmail.com>
2025-12-19 09:35:15 +0500

    src

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,13 @@ public class Main {
         String petSpecies = scanner.nextLine();
         System.out.print("age: ");
         int petAge = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Pet name2: ");
+        String petName2 = scanner.nextLine();
+        System.out.print("Species2: ");
+        String petSpecies2 = scanner.nextLine();
+        System.out.print("Age2: ");
+        int petAge2 = scanner.nextInt();
 
         System.out.print("Adopter name: ");
         scanner.nextLine();
@@ -28,11 +35,18 @@ public class Main {
 
         Pet pet1 = new Pet(petName, petSpecies, petAge);
         System.out.println(pet1.toString());
+        Pet pet2 = new Pet(petName2, petSpecies2, petAge2);
+        System.out.println(pet2.toString());
         Adopter adopter = new Adopter(adopterName, adopterAge, adopterPhoneNumber);
         System.out.println(adopter.toString());
         Shelter shelter = new Shelter(shelterId, shelterName, shelterLocation);
         System.out.println(shelter.toString());
-        }
 
+        if(petAge > petAge2){
+            System.out.println(pet1.getName()+" is older than " + pet2.getName());
+        } else{
+            System.out.println(pet2 + " is older than " + pet1);
+        }
+    }
 }
 
